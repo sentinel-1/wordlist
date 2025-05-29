fd8436b195f21955b0a49f2744084b736abf27ed
Sentinel-1 <sentinel-1@users.noreply.github.com>
2025-05-29 12:25:38 +0400

    Generate Initial Spring Boot project via start.spring.io

---
+++ src/main/java/com/lagogal/wordlist/WordlistApplication.java
@@ -0,0 +1,13 @@
+package com.lagogal.wordlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagogal/wordlist/WordlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagogal.wordlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
